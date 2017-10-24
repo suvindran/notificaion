e4d52c3f5df8008bb6c521b062ecdb5ece2e1b99
Suvindran <suvindran10@gmail.com>
2017-10-24 17:35:00 +0530

    notification

---
+++ app/src/main/java/in/example/firebasecloudsystem/MyFirebaseInstanceIDService.java
@@ -0,0 +1,26 @@
+package in.example.firebasecloudsystem;
+
+import android.app.Service;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by suvindran on 03-Oct-17.
+ */
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseInsIDService";
+
+    @Override
+    public void onTokenRefresh() {
+        //Get updated token
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "New Token: " + refreshedToken);
+
+        //You can save the token into third party server to do anything you want
+    }
+}
+
